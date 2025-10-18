ee9f915a5bbfcdda4daf7b798eb7ec35e6e4f5a5
Максим <shadrin.maksim.sm@gmail.com>
2025-10-18 21:21:25 +0300

    feat(web-host): перейти на минимальный Program, настроить EF Core (SQLite) и Swagger; добавить appsettings.json и обновить .csproj

--- Homeworks/EF/src/PromoCodeFactory.WebHost/Program.cs
+++ Homeworks/EF/src/PromoCodeFactory.WebHost/Program.cs
@@ -1,17 +1,69 @@
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PromoCodeFactory.Core.Abstractions.Repositories;
+using PromoCodeFactory.DataAccess.Data;
+using PromoCodeFactory.DataAccess.DataContext;
+using PromoCodeFactory.DataAccess.Repositories;
 
 namespace PromoCodeFactory.WebHost
 {
-    public class Program
+    public abstract class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+            builder.Services.AddControllers();
+
+            builder.Services.AddDbContext<DataBaseContext>(options =>
+            {
+                options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
+            });
+
+            builder.Services.AddScoped<IDbInitialization, EfDbInitialization>();
+            builder.Services.AddScoped(typeof(IRepository<>), typeof(EfRepository<>));
+
+            builder.Services.AddOpenApiDocument(options =>
+            {
+                options.Title = "PromoCode Factory API Doc";
+                options.Version = "1.0";
+            });
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseOpenApi();
+            app.UseSwaggerUi(settings =>
+            {
+                settings.DocExpansion = "list";
+            });
+
+            app.UseHttpsRedirection();
+
+            app.MapControllers();
+
+            // Dev
+            if (app.Environment.IsDevelopment())
+            {
+                using (var scope = app.Services.CreateScope())
+                {
+                    var init = scope.ServiceProvider.GetRequiredService<IDbInitialization>();
+                    init.Init();
+                }
+            }
+
+            app.Run();
+        }
     }
 }
