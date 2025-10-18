1d48939bf2932d34d4c6328808f39291ff8996e3
Максим <shadrin.maksim.sm@gmail.com>
2025-10-18 23:28:25 +0300

    feat(migrations): добавить миграцию add-customer-nickname и обновить snapshot

---
+++ Homeworks/EF/src/PromoCodeFactory.DataAccess/Migrations/20251030231010_AddCustomerNickname.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PromoCodeFactory.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerNickname : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Customers",
+                type: "TEXT",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Nickname",
+                table: "Customers",
+                type: "TEXT",
+                maxLength: 32,
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("9c0cbb87-6a1d-45f5-a6e8-9f5d7a2e4b10"),
+                column: "Nickname",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Customers",
+                keyColumn: "Id",
+                keyValue: new Guid("a6c8c6b1-4349-45b0-ab31-244740aaf0f0"),
+                column: "Nickname",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Nickname",
+                table: "Customers",
+                column: "Nickname",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Customers_Nickname",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "Nickname",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Customers",
+                type: "TEXT",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 32);
+        }
+    }
+}
