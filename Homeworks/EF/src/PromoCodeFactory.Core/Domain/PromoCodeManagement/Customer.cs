e7086c83146eb61d2c30ddc805af0b6d8184ceeb
Максим <shadrin.maksim.sm@gmail.com>
2025-10-18 23:10:11 +0300

    feat(customer): добавить поле nickname (домен + конфигурация EF)

--- Homeworks/EF/src/PromoCodeFactory.Core/Domain/PromoCodeManagement/Customer.cs
+++ Homeworks/EF/src/PromoCodeFactory.Core/Domain/PromoCodeManagement/Customer.cs
@@ -1,21 +1,19 @@
-﻿using PromoCodeFactory.Core.Domain;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace PromoCodeFactory.Core.Domain.PromoCodeManagement
 {
-    public class Customer
-        : BaseEntity
+    public class Customer : BaseEntity
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string LastName  { get; set; }
 
         public string FullName => $"{FirstName} {LastName}";
 
         public string Email { get; set; }
 
+        public string? Nickname { get; set; }
+
         public ICollection<CustomerPreference> Preferences { get; set; }
-        
-        public ICollection<PromoCode> PromoCodes { get; set; }
+        public ICollection<PromoCode> PromoCodes  { get; set; }
     }
 }
--- Homeworks/EF/src/PromoCodeFactory.DataAccess/Configuration/CustomerConfiguration.cs
+++ Homeworks/EF/src/PromoCodeFactory.DataAccess/Configuration/CustomerConfiguration.cs
@@ -3,24 +3,44 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using PromoCodeFactory.Core.Domain.PromoCodeManagement;
 using PromoCodeFactory.DataAccess.Data;
 
-namespace PromoCodeFactory.DataAccess.Configuration;
-
-public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+namespace PromoCodeFactory.DataAccess.Configuration
 {
-    public void Configure(EntityTypeBuilder<Customer> builder)
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
     {
-        builder.Property(c => c.FirstName)
-            .HasMaxLength(32)
-            .IsRequired();
-        builder.Property(c => c.LastName)
-            .HasMaxLength(64);
-        builder.Property(c => c.Email)
-            .HasMaxLength(256)
-            .IsRequired();
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.Property(c => c.FirstName)
+                .HasMaxLength(32)
+                .IsRequired();
 
-        builder.HasIndex(c => c.Email)
-            .IsUnique();
+            builder.Property(c => c.LastName)
+                .HasMaxLength(32)
+                .IsRequired();
 
-        builder.HasData(FakeDataFactory.Customers);
+            builder.Property(c => c.Email)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            builder.HasIndex(c => c.Email)
+                .IsUnique();
+
+            builder.Property(c => c.Nickname)
+                .HasMaxLength(32);
+
+            builder.HasIndex(c => c.Nickname)
+                .IsUnique();
+
+            builder.HasMany(c => c.Preferences)
+                .WithOne(cp => cp.Customer)
+                .HasForeignKey(cp => cp.CustomerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(c => c.PromoCodes)
+                .WithOne(pc => pc.Customer)
+                .HasForeignKey(pc => pc.CustomerId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasData(FakeDataFactory.Customers);
+        }
     }
 }
